b42c1c55279f927a8f2ae6e7f676bb6b7d3c7cfe
JLussiez <70692644+JLussiez@users.noreply.github.com>
2022-03-13 19:11:43 +0100

    mise en forme de la page capteurs

--- capteurs.php
+++ capteurs.php
@@ -7,6 +7,7 @@
     <title>Station Météo</title>
     <link rel="stylesheet" type="text/css" href="CSS/style.css">
     <link rel="stylesheet" type="text/css" href="CSS/menu.css">
+    <link rel="stylesheet" type="text/css" href="CSS/capteurs.css">
 </head>
 <body> 
     
@@ -14,24 +15,53 @@
         include "menu.php";
     ?>
 
-<div class="text-zone">
-        <h2 class="title">
-            <a href="#">Capteurs</a>
-        </h2>
-    </div>
+    <article class="forecast">
+    <h1>Les différents capteurs de la station</h1>
 
-<div id="icons">
-    <span>Hygromètre</span>
-        <ion-icon name="water-outline"></ion-icon>
-    <span>Capteur de pluie</span>
-        <ion-icon name="rainy-outline"></ion-icon>
-    <span>Capteur Jour/Nuit</span>
-        <ion-icon name="cloudy-night-outline"></ion-icon>
-    <span>Thermomètre</span>
-        <ion-icon name="thermometer-outline"></ion-icon>
-    <span>Baromètre</span>
-        <ion-icon name="trending-up-outline"></ion-icon>
-</div>
+    <article class="day-forecast">
+        <h2>Anémomètre</h2>
+        <p>L'anémomètre sert à mesurer la vitesse du vent.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Baromètre</h2>
+        <p>Le baromètre sert à relever la pression dans l'air.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Capteur de pluie</h2>
+        <p>Le capteur de pluie sert à détecter s'il pleut ou non.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Capteur Jour/Nuit</h2>
+        <p>La capteur Jour/Nuit sert à déterminer s'il fait jour ou nuit.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Girouette</h2>
+        <p>La girouette sert à indiquer le sens du vent.</p>
+    </article>
+    
+    <article class="day-forecast">
+        <h2>Hygromètre</h2>
+        <p>L'hygromètre sert à déterminer le taux d'humidité dans un espace ouvert ou fermé.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Pluviomètre</h2>
+        <p>Le pluviomètre sert à mesurer les précipitations.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Solarimètre</h2>
+        <p>Le Solarimètre sert à mesurer l'intensité lumineuse.</p>
+    </article>
+
+    <article class="day-forecast">
+        <h2>Thermomètre</h2>
+        <p>Le Thermomètre sert à mesurer la température.</p>
+    </article>
 
     <script type="module" src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.esm.js"></script>
     <script nomodule src="https://unpkg.com/ionicons@5.5.2/dist/ionicons/ionicons.js"></script>
