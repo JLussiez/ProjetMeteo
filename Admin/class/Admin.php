b8c8d92093d0e04ca18429e421cca42f009b93bc
BoucherL <louis28.boucher@hotmail.fr>
2022-04-05 16:34:06 +0200

    Variable Class meteo

--- Admin/class/Admin.php
+++ Admin/class/Admin.php
@@ -1,4 +1,5 @@
 <?php
+
  class Admin{
 
     private $_IDAdmin;
--- Admin/class/Meteo.php
+++ Admin/class/Meteo.php
@@ -2,6 +2,21 @@
 
 class Meteo{
 
+    private $_VitesseVent;
+    private $_PositionVent;
+    private $_Pression;
+    private $_Humidite;
+    private $_Temperature;
+    private $_Luminosite;
+    private $_Plviometre;
+    private $_Pluie;
+    private $_Jour_Nuit;
+    private $_Prevision;
+    private $_BDD;
+
+    public function __construct($BDD){
+        $this->_BDD = $BDD;
+    }
 
 
     
