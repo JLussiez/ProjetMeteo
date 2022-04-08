2ada5dc4079be6fc360f4403ab80e240bff021ab
BoucherL <louis28.boucher@hotmail.fr>
2022-04-08 08:59:03 +0200

    formulaire style admin

--- Admin/class/Admin.php
+++ Admin/class/Admin.php
@@ -12,7 +12,37 @@
         $this->_BDD = $BDD;
     }
 
+    
+    public function formUser(){
+    ?>
+    <div class="background">
+        <div class="filtre">
+        <div class="LeForm">
+            <form action="" method="POST">
+                <div class="icone">
+                    <p>👾</p>
+            <!--<img src="img/logo.png"/>-->
+                </div>
+                <h3>LOG IN</h3>
 
+                <input class="champ" type="text" placeholder="Username" name="username" required>
+
+                <input class="champ" type="password" placeholder="Password" name="password" required>
+                <div class="divbtn">
+                    <input class="boutton" type="submit" class='submit' name="BtnLogin" value='Login'>
+                </div>
+
+            </form>
+        </div>
+        </div>
+    </div>
+     
+     
+     
+     <?php
+
+
+    }
 
 
 }
--- Admin/index.php
+++ Admin/index.php
@@ -1,12 +1,22 @@
+<?php
+include "class/Admin.php";
+
+$user = new Admin($BDD);
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style/style.css">
     <title>Gestion Admin</title>
 </head>
 <body>
-    
+    <?php
+        $user->formUser();
+    ?>
 </body>
 </html>
