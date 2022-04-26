bd8c34fef1482e272af691ce6a9c7d5b99af02d4
JLussiez <jlussiez@la-providence.net>
2022-04-26 16:08:11 +0200

    mise a jour

--- JS/websocket.js
+++ JS/websocket.js
@@ -1,7 +1,4 @@
 // Crée une connexion WebSocket
-const socket = new WebSocket('ws://192.168.64.222');
+const socket = new WebSocket("ws://192.168.64.155:16050");
 
 // La connexion est ouverte
-socket.addEventListener('open', (event) => {
-  socket.send('Coucou serveur !');
-});
