9e70d0450b14103c23a757f47a6eae768386f14f
BoucherL <louis28.boucher@hotmail.fr>
2022-05-03 18:05:58 +0200

    afficher donnee capteur

--- Admin/class/Meteo.php
+++ Admin/class/Meteo.php
@@ -1,7 +1,8 @@
 <?php
 
 class Meteo{
-
+    private $_ID;
+    private $_date;
     private $_VitesseVent;
     private $_PositionVent;
     private $_Pression;
@@ -14,12 +15,52 @@ class Meteo{
     private $_Prevision;
     private $_BDD;
 
-    public function __construct($BDD){
-        $this->_BDD = $BDD;
+    public function __construct($bdd){
+        $this->_BDD = $bdd;
+    }
+    public function setParametre($id,$date,$VitesseVent,$PositionVent,$Pression,$Humidite,$Temperature,$Luminosite,$Pluviometre,$Pluie,$JourNuit){
+        $this->_ID = $id;
+        $this->_date = $date;
+        $this->_VitesseVent = $VitesseVent;
+        $this->_PositionVent = $PositionVent;
+        $this->_Pression = $Pression;
+        $this->_Humidite = $Humidite;
+        $this->_Temperature = $Temperature;
+        $this->_Luminosite = $Luminosite;
+        $this->_Pluviometre = $Pluviometre;
+        $this->_Pluie = $Pluie;
+        $this->_Jour_Nuit = $JourNuit;
+
     }
 
+    public function setParametrebyID($ID){
+        $req = "SELECT * FROM `Capteur` WHERE `ID`='".$ID."'";
+        $Result = $this->_BDD->query($req);
+        while($tab = $Result->fetch()){
+            $this->setParametre($tab["ID"], $tab["Date"], $tab["Vitesse_Vent"],$tab["Position_Vent"],
+            $tab["Pression"],$tab["Humidite"],$tab["Temperature"],$tab["Solarimetre"],$tab["Pluviometre"],$tab["Pluie"],$tab["JourNuit"]);
+        }
+    }
 
+    public function affiche(){
+        
+        $req = "SELECT * FROM `Capteur`";
+        $Result = $this->_BDD->query($req);
+        while($tab = $Result->fetch()){
+            echo $tab["ID"];?>
+            <p><?php echo $tab["Date"];?></p>
+            <?php
+            echo $tab["Vitesse_Vent"];
+            echo $tab["Position_Vent"];
+            echo $tab["Pression"];
+            echo $tab["Humidite"];
+            echo $tab["Temperature"];
+            echo $tab["Solarimetre"];
+            echo $tab["Pluviometre"];
+            echo $tab["Pluie"];
+            echo $tab["JourNuit"];
+        }
     
 }
-
+}
 ?>
