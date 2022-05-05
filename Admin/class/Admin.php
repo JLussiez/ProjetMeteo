b090e000853a7232f03149a5cfe3785c8e0b13ed
BoucherL <louis28.boucher@hotmail.fr>
2022-05-05 08:02:59 +0200

    Connexion + formulaire

--- Admin/class/Admin.php
+++ Admin/class/Admin.php
@@ -2,49 +2,98 @@
 
  class Admin{
 
-    private $_IDAdmin;
+    private $_ID;
     private $_Pseudo;
     private $_Mdp;
     private $_BDD;
 
 
-    public function __construct($BDD){
-        $this->_BDD = $BDD;
+    public function __construct($bdd){
+        $this->_BDD = $bdd;
     }
 
+    public function setAdmin($ID,$Pseudo,$Mdp){
+        $this->_ID = $ID;
+        $this->_Pseudo = $Pseudo;
+        $this->_Mdp = $Mdp;
+    }
     
+    public function setAdminbyID($ID){
+        $req = "SELECT * FROM `Admin` WHERE `ID`='".$ID."'";
+        $Result = $this->_BDD->query($req);
+        while($tab = $Result->fetch()){
+            $this->setAdmin($tab["ID"], $tab["Pseudo"], $tab["Password"]);
+        }
+    }
+    public function getID(){
+        return $this->_ID;
+    }
+
+    public function getPseudo(){
+        return $this->_Pseudo;
+    }
+
     public function formUser(){
-    ?>
-    <div class="background">
+
+        $afficheForm = true;
+        $error = false;
+        $_SESSION["Connected"] = false;
+        if(isset($_POST["pseudo"]) && isset($_POST["password"])){
+            $req = "SELECT * FROM `Admin` WHERE `Pseudo`='".$_POST['pseudo']."' AND `Password` = '".$_POST['password']."'";
+            $Result = $this->_BDD->query($req);
+            if($tab = $Result->fetch()){
+                $this->setAdminByID($tab["ID"]);
+                $_SESSION["ID"] = $tab["ID"];
+                $_SESSION["Connected"] = true;
+                $afficheForm = false;
+            }else{
+                $afficheForm = true;
+                $error = true;
+            }
+        }else{
+            $afficheForm = true;
+        }
+
+        if($afficheForm == true){
+            ?>
+            <head>
+                <link rel="stylesheet" href="style/style.css">
+                <link rel="icon" href="img/14.png"/>
+                <title>Gestion Admin</title>
+            </head>
+            <div class="background">
         <div class="filtre">
-        <div class="LeForm">
-            <form action="" method="POST">
-                <div class="icone">
-                    <p>👾</p>
-            <!--<img src="img/logo.png"/>-->
-                </div>
-                <h3>LOG IN</h3>
-
-                <input class="champ" type="text" placeholder="Username" name="username" required>
-
-                <input class="champ" type="password" placeholder="Password" name="password" required>
-                <div class="divbtn">
-                    <input class="boutton" type="submit" class='submit' name="BtnLogin" value='Login'>
-                </div>
-
-            </form>
+    
+    <div class="LeForm">
+        <form action="Gestion.php" method="post">
+        <div class="icone">
+            <p>☀️</p>
         </div>
+        <h3>LOG IN</h3>
+
+        <input class="champ" type="text" placeholder="Username" autocomplete="off" name="pseudo" required>
+
+        <input class="champ" type="password" placeholder="Password" autocomplete="off" name="password" required>
+        <div class="divbtn">
+            <input class="boutton" type="submit" class='submit' name="BtnLogin" value='Login'>
+        </div>    
+        <?php
+            if($error == true){
+        ?><div class="erreur">Pseudo ou Mot de passe incorrect</div><?php
+        }
+        ?>
+    </form>
+
+</div>
         </div>
     </div>
-     
-     
-     
-     <?php
 
+<?php
+        } 
 
     }
 
-
+        
 }
 
 ?>
