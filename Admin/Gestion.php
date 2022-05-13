37d4f99a43739976a68d71cc816c323a7b4d1a1b
BoucherL <louis28.boucher@hotmail.fr>
2022-05-13 15:09:28 +0200

    modif

--- Admin/Gestion.php
+++ Admin/Gestion.php
@@ -2,7 +2,13 @@
 <html lang="en">
 <?php
     include "session.php";
+    if(isset($_POST['supp'])){
+        $meteo->delete($id);
+        header('location: Gestion.php');
+    }
     if($_SESSION["Connected"] == true){
+
+
 ?>
 
 <head>
@@ -10,12 +16,16 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style/table.css">
+    <link rel="stylesheet" href="style/menu.css">
     <link rel="icon" href="img/14.png"/>
     <title>Gestion</title>
 </head>
 <body>
-        <?php
+    <?php
+        include "menu.php";
             $meteo->affiche();
+            $meteo->DateSupp();
+            
         ?>
 </body>
 <?php
